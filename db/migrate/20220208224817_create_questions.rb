6f2d7bc7ddc341ae6056589e2f03bac954f79839
JuicyFlash <tankedet@bk.ru>
2022-02-09 19:42:02 +0300

    - Ограничения объединены с созданием таблиц  - Добавлены связи и внешние ключи

--- db/migrate/20220208224817_create_questions.rb
+++ db/migrate/20220208224817_create_questions.rb
@@ -1,10 +1,11 @@
 class CreateQuestions < ActiveRecord::Migration[7.0]
   def change
     create_table :questions do |t|
-      t.string :body
-      t.integer :test_id
+      t.string :body, null: false
 
       t.timestamps
     end
+    add_reference  :questions, :test, foreign_key: true
+
   end
 end
